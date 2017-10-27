c2f638476595d0e3b6acc83342304b2788da268c
Charlie Shih <shihehe73@gmail.com>
2017-10-27 22:39:30 +0800

    10272239

--- hw2/hw2_xg.py
+++ hw2/hw2_xg.py
@@ -8,23 +8,18 @@ from sklearn.metrics import accuracy_score
 
 feature = [0,1,3,4,5]
 
-def load_data(filename_X,filename_Y):
+def load_data(filename_X):
     data_x = genfromtxt(filename_X, delimiter=',')
-    data_x = data_x[1:,:]
-    data_y = genfromtxt(filename_Y)
-    data_y = data_y[1:]
-    data_x = np.array(data_x)
-    data_x = np.concatenate((data_x,data_x[:, feature] ** 2,data_x[:, feature] ** 3,data_x[:, feature] ** 4,
-                np.log(data_x[:, feature] + 1) ** 2), axis=1)
-    return (data_x,np.array(data_y))
-
-def load_testing_data(filename):
-    data_x = genfromtxt(filename,delimiter=',')
     data_x = data_x[1:,:]
     data_x = np.array(data_x)
     data_x = np.concatenate((data_x,data_x[:, feature] ** 2,data_x[:, feature] ** 3,data_x[:, feature] ** 4,
                 np.log(data_x[:, feature] + 1) ** 2), axis=1)
     return (data_x)
+
+def load_Ydata(filename_Y):
+    data_y = genfromtxt(filename_Y)
+    data_y = data_y[1:]
+    return (np.array(data_y))
 
 def attribute(x):
     return np.mean(x,axis=0), np.std(x, axis=0)
@@ -36,20 +31,18 @@ def bias(x):
     return np.concatenate((np.ones((x.shape[0], 1)), x), axis=1)
 
 if __name__=='__main__':
-    x,y = load_data(sys.argv[1],sys.argv[2])
+    x,y = load_data(sys.argv[1]), load_Ydata(sys.argv[2])
     mean,std = attribute(x)
     x = scale(x,mean,std)
     x = bias(x)
-    test_data = load_testing_data(sys.argv[3])
+    test_data = load_data(sys.argv[3])
     mean, std = attribute(test_data)
     test_data = scale(test_data,mean,std)
     test_data = bias(test_data)
 
-
     model = XGBClassifier(objective= 'binary:logistic',
         learning_rate =0.06,n_estimators=1100)
     model.fit(x,y)
-    model.save_model("./yee.model")
     pred = model.predict(test_data)
     prediction = [round(value) for value in pred]
     id = 0
