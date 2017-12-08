8e7930c45ecc4589c7da90a84948e8872ca1a38c
Charlie Shih <shihehe73@gmail.com>
2017-12-08 14:11:16 +0800

    fix test test_preprocess

--- hw4/hw4_test.sh
+++ hw4/hw4_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget https://www.csie.ntu.edu.tw/~b04902090/hw4_model-06.h5
 wget https://www.csie.ntu.edu.tw/~b04902090/gensim_model
-python data_preprocess.py --test $1
+python test_preprocess.py --test $1
 python predict.py $2
